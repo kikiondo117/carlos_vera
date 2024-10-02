e868da830e21dc5a3f5d1ddeff9ca8fe7f55c650
Carlos Vera <kikiondo117@gmail.com>
2024-10-02 13:52:42 -0600

    Update base route

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/carlos_vera/', // Añadimos la ruta base para que la url sea correcta.
-});
+  base: '/carlos_vera/',
